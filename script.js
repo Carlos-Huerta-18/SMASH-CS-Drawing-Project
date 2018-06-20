f9eef267d5945ee481013a5425db28d50b1aabd4
Carlos <huert1d4@outlook.com>
2018-06-20 10:01:34 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var p5Canvas;
+var myName;
+
+function setup() {
+  p5Canvas = createCanvas(800, 600);
+  p5Canvas.parent("#p5-canvas");
+  myName = select("#my-name");
+  myName.html("[Your Name Here]"); // enter your name here!
+}
+
+ /*
+  full reference: https://p5js.org/reference/
+  line: https://p5js.org/reference/#/p5/line
+  rectangle: https://p5js.org/reference/#/p5/rect
+  ellipse: https://p5js.org/reference/#/p5/ellipse
+  arc: https://p5js.org/reference/#/p5/arc
+  background color: https://p5js.org/reference/#/p5/background
+  shape color: https://p5js.org/reference/#/p5/fill
+  line color, weight, etc: https://p5js.org/reference/#/p5/stroke
+ */
+
+// Write all your code inside the draw() function below!
+function draw() {
+  // Example. You can remove when you're ready!
+  background('blue'); // try changing this to a blue background
+  fill('white');    // Always call fill() before the shape you want to fill in.
+  ellipse(400, 400, 150, 150);
+  ellipse(400, 300, 100, 100);
+  ellipse(400, 2500, 75, 75);
+
+}
